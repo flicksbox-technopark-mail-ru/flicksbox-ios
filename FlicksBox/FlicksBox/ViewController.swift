1b624654a13fdc20d33859f898186d32de5aa806
sn.alekseev <sn.alekseev@corp.vk.com>
2021-03-05 15:37:32 +0300

    create project

---
+++ FlicksBox/FlicksBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlicksBox
+//
+//  Created by sn.alekseev on 05.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
