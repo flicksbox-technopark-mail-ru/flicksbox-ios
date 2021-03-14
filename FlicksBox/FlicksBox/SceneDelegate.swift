cee566725cf11eb0187eae2a16c861603747cac4
Sergei Alexeev <49779711+serenehet@users.noreply.github.com>
2021-03-14 13:11:44 +0300

    FLIC-8: create FactoryViewControllers

--- FlicksBox/FlicksBox/SceneDelegate.swift
+++ FlicksBox/FlicksBox/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let winScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: winScene)
-        window?.rootViewController = StartStep.step()
+        window?.rootViewController = FactoryViewControllers.shared.start()
         window?.makeKeyAndVisible()
     }
 
---
+++ FlicksBox/FlicksBox/ViewControllers/Sign/SignViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignViewController.swift
+//  FlicksBox
+//
+//  Created by sn.alekseev on 14.03.2021.
+//
+
+import UIKit
+
+class SignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
