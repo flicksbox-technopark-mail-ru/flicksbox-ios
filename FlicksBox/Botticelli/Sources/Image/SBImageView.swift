981adf6a777f05f806fb4b9273959101eb7dcb3a
Alkirys <nik_manz@mail.ru>
2021-04-02 11:55:41 +0300

    FLIC-17: Add classes for working with View and images in Botichell

---
+++ FlicksBox/Botticelli/Sources/Image/SBImageView.swift
@@ -0,0 +1,11 @@
+//
+//  SBImageView.swift
+//  Botticelli
+//
+//  Created by Alkirys on 30.03.2021.
+//
+
+import UIKit
+
+open class SBImageView: UIImageView {}
+
---
+++ FlicksBox/Botticelli/Sources/View/SBView.swift
@@ -0,0 +1,18 @@
+//
+//  SBView.swift
+//  Botticelli
+//
+//  Created by Alkirys on 30.03.2021.
+//
+
+import UIKit
+
+open class SBView: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init with coder not implemented")
+    }
+}
