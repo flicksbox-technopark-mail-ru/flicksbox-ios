81d89b23aa7bd57828399363a14eca7d1cf5836d
OlegGibadulin <O.Gibadulin@yandex.ru>
2021-06-05 14:22:36 +0300

    Minor fixes

--- FlicksBox/FlicksBox/ViewControllers/Auth/AuthInput.swift
+++ FlicksBox/FlicksBox/ViewControllers/Auth/AuthInput.swift
@@ -35,18 +35,4 @@ final class AuthIput: UITextField {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    let padding = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
-
-    override public func textRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by: padding)
-    }
-
-    override public func placeholderRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by: padding)
-    }
-
-    override public func editingRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by: padding)
-    }
 }
