182730a7f37920b9abca11c4174e044bc10ec07b
Alkirys <nik_manz@mail.ru>
2021-06-05 13:50:20 +0300

    FLIC-3: Fix paddings in authorization form

--- FlicksBox/FlicksBox/ViewControllers/Auth/AuthInput.swift
+++ FlicksBox/FlicksBox/ViewControllers/Auth/AuthInput.swift
@@ -13,11 +13,26 @@ final class AuthIput: UITextField {
         super.init(frame: frame)
         layer.cornerRadius = 5
         backgroundColor = #colorLiteral(red: 0.9802892804, green: 0.9804533124, blue: 0.9802661538, alpha: 1)
+        tintColor = UIColor.black
         attributedPlaceholder = NSAttributedString(string: "input", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)])
         textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    let padding = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+
+    override public func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override public func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override public func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
     }
 }
